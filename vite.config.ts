64e83339ec2bfc23d20523603a79b724e3bf409b
Evenilson Liandro <evenilsonlp@gmail.com>
2025-06-26 16:14:04 -0300

    fix: correct base path in Vite configuration from '/portifolio/' to '/portfolio/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portifolio/',
+  base: '/portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
